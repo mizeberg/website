6fed161c6ac9b1a981faefaceeb2cf96a7808af3
mizeberg <kasimithil4@gmail.com>
2026-09-19 23:33:48 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/website/',
+ base: '/',
   server: {
     host: "0.0.0.0",
     port: 3000,
